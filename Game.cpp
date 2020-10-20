4dafdb98bf8d2f41d2cdce89cadf32eedd741742
ChristopherValle <53579599+ChristopherValle@users.noreply.github.com>
2020-10-19 21:15:00 -0600

    del commented code

--- Game.cpp
+++ Game.cpp
@@ -5,7 +5,7 @@
 #include <iomanip>
 #include <fstream>
 #include "Game.h"
-//Test
+
 Game::Game() {}
 
 Game::Game(const std::string &name, double price, double tax) : name(name), price(price), tax(tax) {}
@@ -36,31 +36,11 @@ void Game::setTax(double tax) {
     Game::tax = tax;
 }
 
-/* physical only methods: Interface segregation
-double Game::getItemWeight() const {
-    return itemWeight;
-}
 
-void Game::setItemWeight(double itemWeight) {
-    Game::itemWeight = itemWeight;
-}
-
-const std::string &Game::getProductDimensions() const {
-    return productDimensions;
-}
-
-void Game::setProductDimensions(const std::string &productDimensions) {
-    Game::productDimensions = productDimensions;
-}
-*/
 double Game::calculatePriceWithTax() {
     return getPrice() + (getPrice() * getTax());
 }
 
-/**
- * Save the information into a text file
- * @param filename the name of the text file
- */
 void Game::save(const std::string &filename) {
     std::ofstream ofs (filename, std::ofstream::out);
 
@@ -73,8 +53,6 @@ std::string Game::toString() {
     std::ostringstream output;
     output << std::fixed << std::setprecision(2);
     output << "Game Name: " << getName()
-//          << "\nItem Weight: " << getItemWeight()
-//          << "\nProduct Dimensions: " << getProductDimensions()
            << "\nPrice: $" << getPrice()
            << "\nPrice with Tax: $" << calculatePriceWithTax();
 
